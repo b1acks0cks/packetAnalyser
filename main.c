8573dbda2acc13e8445d936b4561bbe0bfd5acf4
Njabulo Mthombeni <nmthombeni965@gmail.com>
2026-02-03 20:40:58 +0200

    Began adding packet reading on an interfaces specified by the user and changing test test() functions to accomodate interfaces

--- main.c
+++ main.c
@@ -7,12 +7,50 @@
 
 
 #include <stdio.h>
+#include<stdlib.h>
 
 
-int main(){
-    //testSegment();
-    testDatagram();
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
+// find a way to read live bytes on a given intercace !!!
 
 
+
+
+int main(int argc, char*argv[] ){
+    
+    char* dev, errbuf[PCAP_ERRBUF_SIZE];
+
+    pcap_if_t **devices = malloc(sizeof(pcap_if_t));
+
+    pcap_findalldevs(devices, errbuf);
+
+    
+    if (devices == NULL){
+        fprintf(stderr, "Malloc error");
+        return(2);
+    }
+    
+    /*
+    for(pcap_if_t *currentinterface = devices[0]; currentinterface != NULL; currentinterface = currentinterface->next){
+        printf("Interfcace %s\n", currentinterface->name);
+    }
+    */
+    
+    for(int i = 0; i < argc; i++){
+        printf("%s \n", argv[i]);
+    }
+
+    free(devices);
     return 0;
 }
